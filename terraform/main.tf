78fd8f8acf2ebda8683745aab7f5d656872b63af
Jean Rodriguez <100055439+jearod@users.noreply.github.com>
2025-01-28 10:38:05 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-west-2"  # Specify your region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"  # Specify your availability zone
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
+  subnet_id     = aws_subnet.main.id
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
